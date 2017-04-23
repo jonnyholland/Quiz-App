fffbb6bc5ae5109357c0e0ed3c4a95ac39ba0e5b
Jon Holland <jonholland@Jonnys-2.local>
2017-04-23 00:11:42 -0400

    First Commit

---
+++ TrueFalseStarter/QuizQuestions.swift
@@ -0,0 +1,36 @@
+//
+//  QuizQuestions.swift
+//  TrueFalseStarter
+//
+//  Created by Jon Holland on 4/22/17.
+//  Copyright © 2017 Treehouse. All rights reserved.
+//
+
+import Foundation
+import GameKit
+
+struct QuizQuestions {
+    let trivia: [[String : String]] = [
+        ["Question": "This was the only US President to serve more than two consecutive terms.", "Answer": "Franklin D. Roosevelt", "Option1": "George Washington", "Option2": "Franklin D. Roosevelt", "Option3": "Woodrow Wilson", "Option4": "Andrew Jackson"],
+        ["Question": "Which of the following countries has the most residents?", "Answer": "Nigeria", "Option1": "Nigeria", "Option2": "Russia", "Option3": "Iran", "Option4": "Vietnam"],
+        ["Question": "In what year was the United Nations founded?", "Answer": "1945", "Option1": "1918", "Option2": "1919", "Option3": "1945", "Option4": "1954"],
+        ["Question": "The Titanic departed from the United Kingdom, where was it supposed to arrive?", "Answer": "New York City", "Option1": "Paris", "Option2": "Washington D.C.", "Option3": "New York City", "Option4": "Boston"],
+        ["Question": "Which nation produces the most oil?", "Answer": "Canada", "Option1": "Iran", "Option2": "Iraq", "Option3": "Brazil", "Option4": "Canada"],
+        ["Question": "Which country has most recently won consecutive World Cups in Soccer?", "Answer": "Brazil", "Option1": "Italy", "Option2": "Brazil", "Option3": "Argetina", "Option4": "Spain"],
+        ["Question": "Which of the following rivers is longest?", "Answer": "Mississippi", "Option1": "Yangtze", "Option2": "Mississippi", "Option3": "Congo", "Option4": "Mekong"  ],
+        ["Question": "Which city is the oldest?", "Answer": "Mexico City", "Option1": "Mexico City", "Option2": "Cape Town", "Option3": "San Juan", "Option4": "Sydney"],
+        ["Question": "Which country was the first to allow women to vote in national elections?", "Answer": "Poland", "Option1": "Poland", "Option2": "United States", "Option3": "Sweden", "Option4": "Senegal"],
+        ["Question": "Which of these countries won the most medals in the 2012 Summer Games?", "Answer": "Great Britian", "Option1": "France", "Option2": "Germany", "Option3": "Japan", "Option4": "Great Britain"]
+    ]
+    
+}
+
+
+
+
+
+
+
+
+
+
